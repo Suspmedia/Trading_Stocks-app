57ce2f0522a8447bb1864778a009c606b97e1677
Suspmedia <suspmedia@gmail.com>
2025-08-01 18:27:44 +0530

    Delete tab5_signal_generation.py

--- tab5_signal_generation.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def run():
-    st.header("🚨 Signal Generator for Option Trades")
-    st.success("Signals for NIFTY, BANKNIFTY, SENSEX based on strategy logic will be displayed.")
